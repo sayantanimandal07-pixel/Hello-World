21611ccae668b55e1b7d9ca85ab9ae5e3a856555
sayantanimandal07-pixel <sayantanimandal07@gmail.com>
2026-09-05 23:19:57 +0530

    hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 print("Hello World")
 
 text = input("Enter a string: ")
-
 reverse = text[::-1]
-
 print("Reversed string:", reverse)
+
+text = input("Enter a string: ")
+
+if text == text[::-1]:
+    print("Palindrome")
+else:
+    print("Not a palindrome")
